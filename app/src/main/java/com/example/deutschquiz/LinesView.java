3f4fa6be1b808953768c9e0bd939fdd7b6770e1d
JC <jcurtelin@insa-rouen.fr>
2025-10-26 09:43:56 +0100

    writeactivity improvement

---
+++ app/src/main/java/com/example/deutschquiz/LinesView.java
@@ -0,0 +1,4 @@
+package com.example.deutschquiz;
+
+public class LinesView {
+}
---
+++ app/src/main/java/com/example/deutschquiz/MenuVerbesActivity.java
@@ -0,0 +1,56 @@
+package com.example.deutschquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuVerbesActivity extends AppCompatActivity {
+    Button button_qcm, button_write, button_list, button_menu, button_guess;
+    TextView name_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        button_qcm = findViewById(R.id.QCM);
+        button_write = findViewById(R.id.Write);
+        button_list = findViewById(R.id.List);
+        button_menu = findViewById(R.id.main_menu);
+        name_text = findViewById(R.id.name_text);
+        button_guess = findViewById(R.id.Guess);
+
+        name_text.setText(getIntent().getStringExtra("destination"));
+
+        button_menu.setOnClickListener(v -> {
+            finish();
+        });
+
+        button_qcm.setOnClickListener(v -> {
+            Intent intent = new Intent(MenuVerbesActivity.this, QCMActivity.class);
+            intent.putExtra("destination", getIntent().getStringExtra("destination"));
+            startActivity(intent);
+        });
+
+        button_write.setOnClickListener(v -> {
+            Intent intent = new Intent(MenuVerbesActivity.this, WriteActivity.class);
+            intent.putExtra("destination", getIntent().getStringExtra("destination"));
+            startActivity(intent);
+        });
+
+        button_list.setOnClickListener(v -> {
+            Intent intent = new Intent(MenuVerbesActivity.this, ListActivity.class);
+            intent.putExtra("destination", getIntent().getStringExtra("destination"));
+            startActivity(intent);
+        });
+
+        button_guess.setOnClickListener(v -> {
+            Intent intent = new Intent(MenuVerbesActivity.this, GuessActivity.class);
+            intent.putExtra("destination", getIntent().getStringExtra("destination"));
+            startActivity(intent);
+        });
+    }
+}
